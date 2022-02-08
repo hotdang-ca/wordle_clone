7218b81223959108c17b34ef9cec95c0e8f96784
James Perih <james@hotdang.ca>
2022-02-07 21:44:59 -0600

    Adjustments to Win Screen

--- lib/widgets/win_screen.dart
+++ lib/widgets/win_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/src/provider.dart';
+import 'package:wordle_clone/logic/guess_provider.dart';
 
 class WinScreen extends StatelessWidget {
   const WinScreen({Key? key}) : super(key: key);
@@ -7,11 +9,33 @@ class WinScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       alignment: Alignment.center,
-      child: const Text(
-        'Winner is you!',
-        style: TextStyle(
-          fontSize: 72,
-        ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 16.0),
+            child: Text(
+              'A winner is you!',
+              style: TextStyle(
+                fontSize: 72,
+              ),
+            ),
+          ),
+          MaterialButton(
+            color: Colors.blue,
+            child: const SizedBox(
+              width: 300,
+              height: 88,
+              child: Center(
+                child: Text(
+                  'New game',
+                  style: TextStyle(fontSize: 32.0, color: Colors.white),
+                ),
+              ),
+            ),
+            onPressed: context.read<GuessProvider>().newGame,
+          ),
+        ],
       ),
     );
   }
